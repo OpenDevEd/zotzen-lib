5cefe210411785b267d9d1f223c60503ca5b2f5c
Björn Haßler <bjohas+git@gmail.com>
2021-01-01 23:07:31 +0000

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
+
 const fs = require('fs');
 
-export async function create(args) {
+async function create(args) {
   const data = fs.readFileSync('data.json', "utf8");
   // console.log(data)
   let result = JSON.parse(data)
