16a32072ff48ed0a3fca23115c234fb86766f06e
ZainaSaadeddin <zeina.sadal.deen@hotmail.com>
2021-01-16 01:08:56 +0200

    first basic functionality is working 1:00am PS

--- test2.js
+++ test2.js
@@ -1,7 +1,16 @@
 const zotzenlib = require('./index')
 async function run() {
     const result = await zotzenlib.create({
-        title: "ABC"
+        "title": "ABC",
+        "authors": "",
+        "description": "",
+        "reportNumber": "",
+        "reportType": "",
+        "date": "",
+        "url": "",
+        "tags": [],
+        "collections": [],
+        "team": ""
     })
     console.log("zotzen result=" + JSON.stringify(result, null, 2))
 }
