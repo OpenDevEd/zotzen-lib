7a2629d43c205304bdda40261a30fe1f5f39a73e
Arfan Liaqat <arfanliaqat10@gmail.com>
2021-05-07 13:07:14 +0500

    feat: update logger
    update logging levels w.r.t args
    change logs file paths

--- src/logger.js
+++ src/logger.js
@@ -1,18 +1,24 @@
 const { createLogger, format, transports } = require('winston');
 
-function getLogingLevel() {
-  let level = 'info';
+const { ArgumentParser } = require('argparse');
 
-  if (process.env.NODE_ENV === 'production') {
-    level = 'error';
-  }
+const parser = new ArgumentParser({
+  add_help: true,
+  description: 'Custom Logs Level CLI',
+});
+parser.add_argument('-v', '--verbose', {
+  help: 'verbose output',
+  action: 'count',
+});
+const args = parser.parse_args();
 
-  if (process.env.NODE_ENV === 'development') {
+function getLogingLevel(config) {
+  let level = 'error';
+
+  const verbose = config.verbose || false;
+
+  if (verbose) {
     level = 'debug';
-  }
-
-  if (process.env.NODE_ENV === 'test') {
-    level = 'error';
   }
 
   return level;
@@ -21,9 +27,19 @@ function getLogingLevel() {
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
-  level: getLogingLevel(),
+  level: getLogingLevel(args),
   format: format.combine(format.splat(), format.simple()),
-  transports: [new transports.Console()],
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new transports.File({ filename: './logs/error.log', level: 'error' }),
+    new transports.File({ filename: './logs/combined.log' }),
+    new transports.Console({
+      format: format.simple(),
+    }),
+  ],
 });
 
 module.exports = logger;
