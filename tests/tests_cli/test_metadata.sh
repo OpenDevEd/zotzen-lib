6cce66192f1bdce3def4fb5474d6d8d81add102b
Björn Haßler <bjohas+git@gmail.com>
2021-05-14 15:16:33 +0100

    zotzen-workspace (tidy up)

---
+++ tests/tests_cli/test_metadata.sh
@@ -0,0 +1,4 @@
+xdg-open zotero://select/groups/2259720/items/6DCTFQPZ
+zotero --show item --key zotero://select/groups/2259720/items/6DCTFQPZ
+zotzen sync --metadata zotero://select/groups/2259720/items/6DCTFQPZ
+zenodo record 811914 
---
+++ tests/tests_cli/upload.sh
@@ -0,0 +1,4 @@
+# zotero item --key zotero://select/groups/2259720/items/2G3NH7VN --addfiles ../test.pdf
+# add tag on zotero
+zotzen sync --attachments --type pdf --tag publishPDF zotero://select/groups/2259720/items/2G3NH7VN
+zotzen sync zotero://select/groups/2259720/items/2G3NH7VN --publish
