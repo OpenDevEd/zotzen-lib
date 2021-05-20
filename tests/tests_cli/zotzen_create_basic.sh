a6af5c81cd5e9fe1bddd8319ed550a4744ddb082
Arfan Liaqat <arfanliaqat10@gmail.com>
2021-05-20 15:46:40 +0500

    fix: fix test

--- tests/tests_cli/zotzen_create_basic.sh
+++ tests/tests_cli/zotzen_create_basic.sh
@@ -1,1 +1,1 @@
-zotzen create --title "My title - CLI" --group-id 2259720 --date 2021 --kerko-url https://docs.edtechhub.org/lib/ --authors "LAST, FIRST; LAST2, FIRST2"
+zotzen create --title "My title - CLI" --group-id 2259720 --date 2021 --kerko-url https://docs.edtechhub.org/lib/ --authors "LAST, FIRST" "LAST2, FIRST2"
